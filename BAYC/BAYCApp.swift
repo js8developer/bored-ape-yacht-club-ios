6009d7e30999154b587ba45983a869119d1af920
JasonSchneider8 <36863375+JasonSchneider8@users.noreply.github.com>
2022-08-19 16:23:37 -0400

    Initial Commit

---
+++ BAYC/BAYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAYCApp.swift
+//  BAYC
+//
+//  Created by Jason Schneider on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BAYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
