42f538802cb87d1b2e6fb997abcd665d55e06811
JasonSchneider8 <36863375+JasonSchneider8@users.noreply.github.com>
2023-01-12 23:14:05 -0500

    Create README.md

---
+++ BAYC/Domain/Entities/OpenSeaResponse.swift
@@ -0,0 +1,13 @@
+//
+//  OpenSeaResponse.swift
+//  BAYC
+//
+//  Created by Jason Schneider on 8/20/22.
+//
+
+import Foundation
+
+struct OpenSeaResponse: Codable {
+    let stats: OpenSeaCollectionStats
+}
+
---
+++ BAYC/README.md
@@ -0,0 +1,4 @@
+# bored-ape-yacht-club-ios
+iOS Remix of the Bored Ape Yacht Club Website 
+
+
